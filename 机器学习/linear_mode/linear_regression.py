c6fac654d6e439f0f67a6ae27bb8fa65f4cbb3d7
joestarhu <56381125+joestarhu@users.noreply.github.com>
2020-03-23 14:59:55 +0800

    Update linear_regression.py

--- 机器学习/linear_mode/linear_regression.py
+++ 机器学习/linear_mode/linear_regression.py
@@ -1,70 +1,95 @@
 import numpy as np
 
-class LinearRegression:
+class Linear_Regression:
     def __init__(self,fit_inter=True):
-        self.fit_inter = fit_inter
+        self.__fit_inter = True
 
-    def __intercept_fit(self,X):
-        """输入匹配截距"""
-        if self.fit_inter:
+    def __inter_fit(self,X):
+        if self.__fit_inter:
             X = np.c_[np.ones(X.shape[0]),X]
         return X
 
     def __weight_set(self,W):
-        """截距和权重参数设置"""
-        W = W.flatten()
-        self.inter_ = W[0] if self.fit_inter else 0.
-        self.coef_ = W[self.fit_inter:]
+        self.inter_ = W[0] if self.__fit_inter else 0.
+        self.coef_ = W[self.__fit_inter:]
 
     def __normal_equation(self,X,y):
-        """正规方程拟合"""
-        X = self.__intercept_fit(X)
-        self.W = np.linalg.pinv(X.T @ X) @ X.T @ y
+        X = self.__inter_fit(X)
+        # 考虑到矩阵不可逆的情况,用违逆函数
+        self.W  = np.linalg.pinv(X.T @ X) @ X.T @ y
         self.__weight_set(self.W)
 
-    def __mean_square_error(self,X,y):
-        """代价函数:均方差MSE"""
-        return np.sum((self.predict(X) - y)**2)/X.shape[0]
-
     def __gradient_descent(self,X,y,W,lr,itercnt):
-        """梯度下降拟合参数"""
-        delta = 1e-7
-        self.W = W.copy()
+        """
+        代价函数使用:MSE(Mean Square Error)
+        求导有2种实现方式:
+        1) 直接写出MSE求导后的公式实现
+        2) 使用数值微分的方式实现
+        """
+        self.W = W
         grad = np.zeros_like(W)
 
+        """
+        --------------------------
+        1) 直接写出MSE求导后的公式实现
+        --------------------------
+        """
+        X = self.__inter_fit(X)
+        for _ in range(itercnt):
+            for i in range(grad.shape[0]):
+                grad[i] = np.sum((X @ self.W - y) * X[:,i]) / X.shape[0]
+            # 梯度下降要同时更新
+            update = lr*grad
+
+            # 如果更新值过小or过大,认为下降已经无意义结束算法
+            if np.all(np.abs(update) < 1e-7) or np.any(np.abs(update) > 1e7):
+                break
+            self.W -= update
+        self.__weight_set(self.W)
+        """
+        --------------------------
+        2) 使用数值微分的方式实现
+        --------------------------
+        """
+        """
         for _ in range(itercnt):
             for i in range(grad.shape[0]):
                 t = self.W[i]
-                self.W[i] = t + delta
-                f1 = self.__mean_square_error(X,y)
-                self.W[i] = t - delta
-                f2 = self.__mean_square_error(X,y)
-                grad[i] = (f1-f2)/(2*delta)
+                self.W[i] = t + 1e-7
+                f1 = np.sum((self.predict(X) - y)**2) / 2 / X.shape[0]
+                self.W[i] = t - 1e-7
+                f2 = np.sum((self.predict(X) - y)**2) / 2 / X.shape[0]
+                grad[i] = (f1-f2)/(2*1e-7)
                 self.W[i] = t
             update = lr*grad
-
-            # 如果收敛或者更新过大,结束优化
-            if np.all(np.abs(update) < 1e-7) or np.any(np.abs(update) > 1e7):break
+            if np.all(np.abs(update) < 1e-7) or np.any(np.abs(update) > 1e7):
+                break
             self.W -= update
         self.__weight_set(self.W)
+        """
 
-    def fit(self,X,y,W=None,lr=0.01,itercnt=1e5):
-        """拟合参数"""
+    def fit(self,X,y,W=None,lr=0.01,itercnt = 1e5):
+        # 如果没有输入预先设定的权重参数,那么就使用正规方程拟合
+
+        # 调整shape,让后续的运算不会出现shape不一致的错误
+        y = y.flatten()
+
         if W is None:
             self.__normal_equation(X,y)
         else:
+            # 调整shape,让后续的运算不会出现shape不一致的错误
+            W = W.copy().flatten()
             itercnt = int(itercnt)
             self.__gradient_descent(X,y,W,lr,itercnt)
 
     def predict(self,X):
-        """线性回归预测输出"""
-        return self.__intercept_fit(X) @ self.W
+        return self.__inter_fit(X) @ self.W
 
 if __name__ == '__main__':
-    a = np.array([1,2,3,4])
+    a = np.arange(10)
     w = np.array([0.1,0.2])
-    y = 1+2*a
-    lr = LinearRegression()
-    lr.fit(a,y)
+    y = (1 + 2*a).reshape(-1,1)
+    lr = Linear_Regression()
     lr.fit(a,y,w)
-    print(lr.inter_,lr.coef_)
+    lr.W
+    lr.predict(a)
