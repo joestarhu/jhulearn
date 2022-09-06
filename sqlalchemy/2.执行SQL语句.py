e8dcf3fb6be10111eac92ccb5734c00868b47062
joestarhu <56381125+joestarhu@users.noreply.github.com>
2022-09-06 15:48:25 +0800

    Update 2.执行SQL语句.py

--- sqlalchemy/2.执行SQL语句.py
+++ sqlalchemy/2.执行SQL语句.py
@@ -1,39 +1,49 @@
 from sqlalchemy import create_engine,text
-from urllib.parse import quote
 
-# name=quote('root')
-# passwd = quote('qwe321')
-# host=quote('localhost')
-# port='55001'
 uri = 'mysql+pymysql://root:qwe321@localhost:55001/jhu?charset=utf8mb4'
-
 engine = create_engine(uri,echo=True,future=True)
 
 # 创建表格
-stmt = text('create table some_table(id integer,name varchar(256))')
-with engine.connect() as conn:
-    conn.execute(stmt)
-
-# 插入数据 单个数据
-stmt=text('insert into some_table values(:id,:name)').bindparams(id=1,name='hujian')
+stmt = text('create table demo_user(id integer, name varchar(256))')
 with engine.connect() as conn:
     conn.execute(stmt)
     conn.commit()
 
-# 插入数据 多个数据
-stmt=text('insert into some_table values(:id,:name)')
+# 插入单行数据
+stmt = text("insert into demo_user values(:id,:name)").bindparams(id=1,name='jack')
 with engine.connect() as conn:
-    conn.execute(stmt,[dict(id=2,name='jack'),dict(id=3,name='rose'),dict(id=4,name='eva')])
+    conn.execute(stmt)
     conn.commit()
+
+# 插入多行数据
+stmt = text("insert into demo_user values(:id,:name)")
+with engine.connect() as conn:
+    conn.execute(stmt,[dict(id=2,name='rose'),dict(id=3,name='eva')])
+    conn.commit()a
 
 # 查询数据
-stmt=text('select * from some_table')
+stmt = text('select * from demo_user')
 with engine.connect() as conn:
     result = conn.execute(stmt)
-    print(result.all())
+    for row in result:
+        print(row.id,row.name)
+        print(row['id'],row['name'])
+    for row in result.mappings():
+        print(row['id'])
 
-# 删除表格
-stmt=text('drop table some_table')
+# 单行更新&批量更新
+stmt = text('update demo_user set name=:name where id=:id').bindparams(id=1,name='jack-1')
+with engine.connect() as conn:
+    conn.execute(stmt)
+    conn.commit()
+
+stmt = text('update demo_user set name=:name where id=:id')
+with engine.connect() as conn:
+    conn.execute(stmt,[dict(id=2,name='rose-2'),dict(id=3,name='eva-3')])
+    conn.commit()
+
+# 移除表格
+stmt = text('drop table demo_user')
 with engine.connect() as conn:
     conn.execute(stmt)
     conn.commit()
